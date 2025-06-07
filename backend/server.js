5d70b2ff82c4c3b5dd586b8d0b03ebe21b0dd944
Prashant Timilsina <157892231+PrashantTimilsina@users.noreply.github.com>
2025-06-07 17:55:56 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,13 +19,13 @@ dotenv.config();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://rental-frontend-ndxp.onrender.com",
     credentials: true,
   },
 });
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://rental-frontend-ndxp.onrender.com",
     credentials: true,
   })
 );
